ea3b2bc789fb426c0e13535c2c5881035093d902
Kazuki Maeda <kmaeda@kmaeda.net>
2016-05-12 21:04:34 +0900

    create_archive.sh: uptex -> uptex-fonts.

--- create_archive.sh
+++ create_archive.sh
@@ -16,32 +16,32 @@ rm $TMP/$PROJECT/create_archive.sh
 perl -pi.bak -e "s/\\\$RELEASEDATE/$RELEASEDATE/g" $TMP/$PROJECT/README.md
 rm -f $TMP/$PROJECT/README.md.bak
 
-mkdir -p $TMP/$PROJECT/doc/fonts/uptex
-mv $TMP/$PROJECT/LICENSE $TMP/$PROJECT/doc/fonts/uptex/
-mv $TMP/$PROJECT/README.md $TMP/$PROJECT/doc/fonts/uptex/
-mv $TMP/$PROJECT/*.txt $TMP/$PROJECT/doc/fonts/uptex/
+mkdir -p $TMP/$PROJECT/doc/fonts/$PROJECT
+mv $TMP/$PROJECT/LICENSE $TMP/$PROJECT/doc/fonts/$PROJECT/
+mv $TMP/$PROJECT/README.md $TMP/$PROJECT/doc/fonts/$PROJECT/
+mv $TMP/$PROJECT/*.txt $TMP/$PROJECT/doc/fonts/$PROJECT/
 
-mkdir -p $TMP/$PROJECT/fonts/cmap/uptex
-mv $TMP/$PROJECT/cmap/* $TMP/$PROJECT/fonts/cmap/uptex
+mkdir -p $TMP/$PROJECT/fonts/cmap/$PROJECT
+mv $TMP/$PROJECT/cmap/* $TMP/$PROJECT/fonts/cmap/$PROJECT
 
-mkdir -p $TMP/$PROJECT/source-new/fonts/uptex
-mv $TMP/$PROJECT/source/* $TMP/$PROJECT/source-new/fonts/uptex/
+mkdir -p $TMP/$PROJECT/source-new/fonts/$PROJECT
+mv $TMP/$PROJECT/source/* $TMP/$PROJECT/source-new/fonts/$PROJECT/
 rmdir $TMP/$PROJECT/source
 mv $TMP/$PROJECT/source{-new,}
 
-mkdir -p $TMP/$PROJECT/fonts/tfm/uptex/min
-mv $TMP/$PROJECT/tfm/ugb* $TMP/$PROJECT/fonts/tfm/uptex/min/
-mv $TMP/$PROJECT/tfm/urml* $TMP/$PROJECT/fonts/tfm/uptex/min/
-mv $TMP/$PROJECT/tfm/*10* $TMP/$PROJECT/fonts/tfm/uptex/min/
+mkdir -p $TMP/$PROJECT/fonts/tfm/$PROJECT/min
+mv $TMP/$PROJECT/tfm/ugb* $TMP/$PROJECT/fonts/tfm/$PROJECT/min/
+mv $TMP/$PROJECT/tfm/urml* $TMP/$PROJECT/fonts/tfm/$PROJECT/min/
+mv $TMP/$PROJECT/tfm/*10* $TMP/$PROJECT/fonts/tfm/$PROJECT/min/
 
-mkdir -p $TMP/$PROJECT/fonts/tfm/uptex/jis
-mv $TMP/$PROJECT/tfm/* $TMP/$PROJECT/fonts/tfm/uptex/jis
+mkdir -p $TMP/$PROJECT/fonts/tfm/$PROJECT/jis
+mv $TMP/$PROJECT/tfm/* $TMP/$PROJECT/fonts/tfm/$PROJECT/jis
 
-mkdir -p $TMP/$PROJECT/fonts/vf/uptex/min
-mv $TMP/$PROJECT/vf/*10* $TMP/$PROJECT/fonts/vf/uptex/min
+mkdir -p $TMP/$PROJECT/fonts/vf/$PROJECT/min
+mv $TMP/$PROJECT/vf/*10* $TMP/$PROJECT/fonts/vf/$PROJECT/min
 
-mkdir -p $TMP/$PROJECT/fonts/vf/uptex/jis
-mv $TMP/$PROJECT/vf/* $TMP/$PROJECT/fonts/vf/uptex/jis
+mkdir -p $TMP/$PROJECT/fonts/vf/$PROJECT/jis
+mv $TMP/$PROJECT/vf/* $TMP/$PROJECT/fonts/vf/$PROJECT/jis
 
 rmdir $TMP/$PROJECT/{cmap,tfm,vf}
 

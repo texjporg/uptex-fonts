01c01469426f55747ba1190fb4168a4563ce7899
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2023-01-28 12:46:06 +0900

    create_archive.sh: tweak

--- create_archive.sh
+++ create_archive.sh
@@ -67,6 +67,7 @@ rm $TMP/$PROJECT/.gitignore
 rm $TMP/$PROJECT/create_archive.sh
 rm $TMP/$PROJECT/Makefile
 rm -rf $TMP/$PROJECT/00uptex-0.30
+rm -rf $TMP/$PROJECT/samples
 perl -pi.bak -e "s/\\\$RELEASEDATE/$RELEASEDATE/g" $TMP/$PROJECT/README.md
 rm -f $TMP/$PROJECT/README.md.bak
 
